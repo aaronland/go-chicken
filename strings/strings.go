f8bf998be79773604eda41d0cc8ff17bbc97aa7f
thisisaaroland <aaron@mapzen.com>
2016-05-26 17:21:53 -0700

    clucking in hbo, spa and jpn

--- strings/strings.go
+++ strings/strings.go
@@ -16,5 +16,8 @@ var CHICKENS = map[string]string{
 var CLUCKING = map[string][]string{
 	"eng": []string{"cluck cluck", "bok bok bok", "bok bok b'gawk"},
 	"fre": []string{"cot cot cot", "cot cot codet"},
+	"hbo": []string{"קוּר־קוּר־קוּר","בָּק־בָּק־בָּק"},
+	"jpn": []string{"コッコッ"},
 	"pol": []string{"ko ko"},
+	"spa": []string{"kikiriki"},
 }
